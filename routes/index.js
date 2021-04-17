55e2ec51a746a3de2ea0c4fb940bf6be321b37a0
Piyush <piyushranjan1402@gmail.com>
2021-04-18 01:06:18 +0530

    fix bugs in front page and Improve style of news

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,6 @@ var rawData = fs.readFileSync(path.join(__dirname,"..","data", 'players.json'));
 // var rawData = fs.readFileSync(path.join(__dirname,"..","data", 'sample_search_2.json'));
 var players = JSON.parse(rawData);
 
-var d = new Date("1981-07-07T00:00Z");
-console.log(d.getDate());
-console.log(d.getMonth());
-
-
-
-
-
-
 router.get("/",function (req,res) {
   let content="text content"
 
@@ -54,6 +45,7 @@ router.get("/search",function(req,res){
    
   // let wordToMatch=req.body.search__input;
   let wordToMatch=req.query.search__input;
+  wordToMatch=wordToMatch.replace(/\s+/g, ' ').trim();
 
   var start = new Date()
   let limit=players.length;
@@ -63,7 +55,7 @@ router.get("/search",function(req,res){
   let searchObject=processMatchArray(wordToMatch,limit);//wordToMatch,limit=first 100 players  
   let noOfResults=Object.keys(searchObject).length;
   // console.log(typeof searchObject);
-  searchObject
+  // searchObject
   var end = (new Date() - start)/1000;//to convert ms to s 
   // console.info('Execution time:%ds', (end/1000)) 
     // res.send("hello")
