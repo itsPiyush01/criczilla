5b2f6c9db4b63fd62a16b836e1a2275b904c3128
Piyush <piyushranjan1402@gmail.com>
2021-04-21 08:58:19 +0530

    Delete try catch block in player.js

--- routes/player.js
+++ routes/player.js
@@ -15,29 +15,11 @@ router.get("/:id",async(req,res) => {
     let result=await findOne(_id);
     if(result)
     {
-        //  result = Object.keys(result).map(function (key) { return result[key]; });
-        // console.log(result);
-        try {
-            //... prepare context
-            // return res.render('template.ejs', context);
-            // return next(new MyCustomError('template failed:' + e.message));
-                res.render("player",{searchedWord:"",player:result})
-
-          }
-          catch(e)
-          {
-              res.render("<h1>OPS...</h1>")
-            // here you can analyze the cause of error and pass useful info to
-            // error handler
-          }
         res.render("player",{searchedWord:"",player:result})
     }
-    else{
-        // res.send("error")
+    else{     
         res.status(404).send("<h1>NOT FOUND</h1><p>Something Went Wrong :(</p>");
-        // res.err(404);
-        // next(err) // Pass errors to Express.
-   
+
     }
 })
 
