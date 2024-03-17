88a1462f1bffd485a7a83675961398643b9c295b
itsPiyush01 <piyushranjan1402@gmail.com>
2024-03-17 17:35:08 +0530

    add certificate and Presentation

--- routes/index.js
+++ routes/index.js
@@ -1,143 +1,148 @@
 // const { assert } = require('console');
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
-const fs = require('fs');
+var path = require("path");
+const fs = require("fs");
 
-var MongoClient = require('mongodb').MongoClient;
-var assert =require("assert");
+var MongoClient = require("mongodb").MongoClient;
+var assert = require("assert");
 const url = "mongodb://127.0.0.1:27017";
 
-var rawData = fs.readFileSync(path.join(__dirname,"..","data", 'players.json'));
+var rawData = fs.readFileSync(
+	path.join(__dirname, "..", "data", "players.json")
+);
 // var rawData = fs.readFileSync(path.join(__dirname,"..","data", 'sample_search_2.json'));
 var players = JSON.parse(rawData);
 
-router.get("/",function (req,res) {
-  let content="text content"
+router.get("/", function (req, res) {
+	let content = "text content";
 
-  // console.log(players);
-  let playersTodayBirthday=[];
-  let searchLimit=players.length;//no limit
+	// console.log(players);
+	let playersTodayBirthday = [];
+	let searchLimit = players.length; //no limit
 
-  
-  var today = new Date();
-  // convertTZ("2012/04/10 10:10:30 +0000", "/Asia/Kolkata") 
-  // console.log(today);
-  let IndiaTime = convertTZ(today,"Asia/Kolkata");
-  currentDate=IndiaTime.getDate();
-  currentMonth=IndiaTime.getMonth()+1;
-  
+	var today = new Date();
+	// convertTZ("2012/04/10 10:10:30 +0000", "/Asia/Kolkata")
+	// console.log(today);
+	let IndiaTime = convertTZ(today, "Asia/Kolkata");
+	currentDate = IndiaTime.getDate();
+	currentMonth = IndiaTime.getMonth() + 1;
 
-  for(let i=0;i<players.length && playersTodayBirthday.length<searchLimit ;i++)
-    {
-      if(players[i].date_of_birth && players[i].date_of_birth!="null" )
-      {
-            let date=new Date(players[i].date_of_birth);
-            let playerBirthDate=date.getDate();
-            let playerBirthMonth=date.getMonth()+1;
-            if(playerBirthDate==currentDate && playerBirthMonth==currentMonth)
-            {
-              playersTodayBirthday.push(players[i]);
-            }
-      }   
-    }
-    // playersTodayBirthday
+	for (
+		let i = 0;
+		i < players.length && playersTodayBirthday.length < searchLimit;
+		i++
+	) {
+		if (players[i].date_of_birth && players[i].date_of_birth != "null") {
+			let date = new Date(players[i].date_of_birth);
+			let playerBirthDate = date.getDate();
+			let playerBirthMonth = date.getMonth() + 1;
+			if (playerBirthDate == currentDate && playerBirthMonth == currentMonth) {
+				playersTodayBirthday.push(players[i]);
+			}
+		}
+	}
+	// playersTodayBirthday
 
-    function compare( a, b ) {
-      if ( a.name < b.name ){
-        return -1;
-      }
-      if ( a.name > b.name ){
-        return 1;
-      }
-      return 0;
-    }
-    
-    playersTodayBirthday.sort( compare );
-    // console.log(playersTodayBirthday);
+	function compare(a, b) {
+		if (a.name < b.name) {
+			return -1;
+		}
+		if (a.name > b.name) {
+			return 1;
+		}
+		return 0;
+	}
 
-    // console.log(playersTodayBirthday.length);
-    res.render("index",{players:playersTodayBirthday,searchedWord:""})
-  
-  // var searchQuery=
-  // const username = req.body.username
-  //...
-  // res.end()
-})
+	playersTodayBirthday.sort(compare);
+	// console.log(playersTodayBirthday);
 
-router.post("/",(req,res)=>{
-  // res.
-  // console.log(req.baseUrl);
+	// console.log(playersTodayBirthday.length);
+	res.render("index", { players: playersTodayBirthday, searchedWord: "" });
 
-  // console.log(req.body.);
-  // res.send("hello")
+	// var searchQuery=
+	// const username = req.body.username
+	//...
+	// res.end()
+});
 
+router.post("/", (req, res) => {
+	// res.
+	// console.log(req.baseUrl);
+	// console.log(req.body.);
+	// res.send("hello")
+});
 
-})
+router.post("/searchSuggestionQuery", function (req, res) {
+	let wordToMatch = req.body.wordToMatch;
+	let searchObject = processMatchArray(wordToMatch, 5); //wordToMatch,limit
+	res.send(searchObject);
+});
 
+router.get("/search", function (req, res) {
+	// let wordToMatch=req.body.search__input;
+	let wordToMatch = req.query.search__input;
+	wordToMatch = wordToMatch.replace(/\s+/g, " ").trim();
 
-router.post("/searchSuggestionQuery",function(req,res){
-  let wordToMatch=req.body.wordToMatch;
-  let searchObject=processMatchArray(wordToMatch,5);//wordToMatch,limit
-  res.send(searchObject);
-})
+	var start = new Date();
+	let limit = players.length;
+	// let limit=100;
 
-router.get("/search",function(req,res){
-   
-  // let wordToMatch=req.body.search__input;
-  let wordToMatch=req.query.search__input;
-  wordToMatch=wordToMatch.replace(/\s+/g, ' ').trim();
+	// execution time for processMatchArray function
+	let searchObject = processMatchArray(wordToMatch, limit); //wordToMatch,limit=first 100 players
+	let noOfResults = Object.keys(searchObject).length;
+	// console.log(typeof searchObject);
+	// searchObject
+	var end = (new Date() - start) / 1000; //to convert ms to s
+	// console.info('Execution time:%ds', (end/1000))
+	// res.send("hello")
+	var resultStats = {
+		noOfResults: noOfResults,
+		time: end,
+	};
 
-  var start = new Date()
-  let limit=players.length;
-  // let limit=100;
-  
-  // execution time for processMatchArray function
-  let searchObject=processMatchArray(wordToMatch,limit);//wordToMatch,limit=first 100 players  
-  let noOfResults=Object.keys(searchObject).length;
-  // console.log(typeof searchObject);
-  // searchObject
-  var end = (new Date() - start)/1000;//to convert ms to s 
-  // console.info('Execution time:%ds', (end/1000)) 
-    // res.send("hello")
-    var resultStats={
-      noOfResults:noOfResults,
-      time:end
-    }
+	// console.log(resultStats.noOfResults+" "+resultStats.time);
 
-    // console.log(resultStats.noOfResults+" "+resultStats.time);
-    
+	res.render("searchPage", {
+		players: searchObject,
+		title: "Test Heading",
+		searchedWord: wordToMatch,
+		resultStats: resultStats,
+	});
+});
 
-    res.render("searchPage",{players:searchObject,title:"Test Heading",searchedWord:wordToMatch,resultStats:resultStats})
- })
+function processMatchArray(wordToMatch, searchLimit) {
+	let matchArray = [];
 
+	if ((undefined = !wordToMatch || wordToMatch != "")) {
+		const regex = new RegExp(wordToMatch, "gi");
+		for (
+			let i = 0;
+			i < players.length && matchArray.length < searchLimit;
+			i++
+		) {
+			let playerFullName = players[i].full_name;
+			let playerName = players[i].name;
+			if (playerFullName == null) playerFullName = "";
+			if (playerName == null) playerName = "";
 
-
-function processMatchArray(wordToMatch,searchLimit) {
-   let matchArray=[];
-  
-  if(undefined=!wordToMatch || wordToMatch!="")
-  {
-      const regex= new RegExp(wordToMatch,'gi');
-      for(let i=0;i<players.length && matchArray.length<searchLimit ;i++)
-      {
-          let playerFullName=players[i].full_name;
-          let playerName=players[i].name;
-          if(playerFullName==null)playerFullName="";
-          if(playerName==null)playerName="";
-          
-          if(matchArray.length<searchLimit && 
-          (playerFullName.match(regex) || 
-          playerName.match(regex)))
-          {matchArray.push(players[i])};      
-      }
-  }
-      return  matchArray;
+			if (
+				matchArray.length < searchLimit &&
+				(playerFullName.match(regex) || playerName.match(regex))
+			) {
+				matchArray.push(players[i]);
+			}
+		}
+	}
+	return matchArray;
 }
 
-
 function convertTZ(date, tzString) {
-  return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+	return new Date(
+		(typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {
+			timeZone: tzString,
+		})
+	);
 }
 
 // router.all("/live",function (req,res) {
@@ -155,7 +160,6 @@ function convertTZ(date, tzString) {
 
 // })
 
-
 // router.all("/news",function (req,res) {
 //   let content="NEWS"
 //   // console.log(players);
@@ -163,11 +167,4 @@ function convertTZ(date, tzString) {
 
 // })
 
-
-
-
-
 module.exports = router;
-
-
-
